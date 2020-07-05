bbbc5ce44801754a969c50f2d540e86b9ebed2c9
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2020-07-05 22:21:42 +0500

    Some refactoring

--- git.bat
+++ git.bat
@@ -1,6 +1,9 @@
 cd temp/vol
+set arg1=%1
+set arg2=%2
 git init
 git add .
-git remote add origin https://github.com/AcipenserSturio/test_repo.git
+git remote add origin https://github.com/%arg1%/%arg2%.git
 git commit -m "Test files"
 git push -u origin master
+pause
--- main.py
+++ main.py
@@ -1,68 +1,91 @@
-import subprocess, os, urllib.request
+import subprocess, os, urllib.request, json
 
-input_file = 'archivedtwice.zip'
-output_folder = 'temp/fromWayback'
-temp_file_name = 'temp/temp.txt'
-downloaded_ascii = 'temp_transformed'
-final_output = 'regained.zip'
-urls = ["https://web.archive.org/web/20200621143517if_/https://acipensersturio.github.io/test_repo/1.html",
-        "https://web.archive.org/web/20200621143622if_/https://acipensersturio.github.io/test_repo/2.html",
-        "https://web.archive.org/web/20200621134704if_/https://acipensersturio.github.io/test_repo/3.html",
-        "https://web.archive.org/web/20200621141255if_/https://acipensersturio.github.io/test_repo/4.html"]
-def get_page(url, filename):
+github_account = "acipensersturio"
+github_repo = "test_repo_4"
+path_temp_folder = "temp/"
+path_vol_folder = "temp/vol/"
+path_json_folder = "temp/json/"
+path_outer = "in/outer.zip"
+path_ascii = "temp/ascii.txt"
+path_out = "out/outer.zip"
+files_count = 101
+urls = []
+
+def get_page(url, filepath):
     contents = urllib.request.urlopen(url).read()
-    with open(filename, 'wb') as file:
+    with open(filepath, 'wb') as file:
         file.write(contents)
+def save_page(url):
+    contents = urllib.request.urlopen(url).read()
+    print(url, "saved")
 
 # Method used for splitting files
-def split_file(input_file, output_folder, output_file_name='', output_file_ext = ".html", chunk_size = 2**20):
+def split_file(input_file, output_folder, output_file_ext = ".html", chunk_size = 2**20):
     file_number = 0
     with open(input_file, 'rb') as f:
         chunk = f.read(chunk_size)
         while chunk != b'':
-            file_path = output_folder + "/" + output_file_name + str(file_number) + output_file_ext
+            file_path = output_folder + str(file_number) + output_file_ext
             with open(file_path, 'wb') as chunk_file:
                 chunk_file.write(chunk)
             file_number += 1
             chunk = f.read(chunk_size)
     return file_number
 
-def join_files(file_number, output_file, input_folder, input_file_name='', input_file_ext = ".txt"):
+def join_files(file_number, output_file, input_folder, input_file_ext = ".html"):
     with open(output_file, 'wb') as out:
         for file in range(file_number):
-            file_path = input_folder + "/" + input_file_name + str(file) + input_file_ext
+            file_path = input_folder + str(file) + input_file_ext
             with open(file_path, 'rb') as f:
                 chunk = f.read()
                 print(out.write(chunk))
 
-def file_to_ascii_to_github():
-    # Create output folder, if it doesn't already exist
-    if not os.path.exists(output_folder):
-        os.mkdir(output_folder)
-    # Get ascii from file
-    subprocess.run("encode.exe " + input_file + " " + temp_file_name)
-    # Split ascii into multiple html pages
-    split_file(temp_file_name, output_folder, chunk_size = 2**23)
-    # Remove ascii
-    os.remove(temp_file_name)
-    # Upload html pages to github
-    subprocess.run("git.bat")
 
-def ascii_to_file():
+def uploading_to_wayback():
+    # Create output folder, if it doesn't already exist
+    if not os.path.exists(path_temp_folder):
+        os.mkdir(path_temp_folder)
+    if not os.path.exists(path_vol_folder):
+        os.mkdir(path_vol_folder)
+    # Get ascii from file
+    subprocess.run("encode.exe " + path_outer + " " + path_ascii)
+    # Split ascii into multiple html pages
+    split_file(path_ascii, path_vol_folder, chunk_size = 2**20)
+    # Remove ascii
+    os.remove(path_ascii)
+    # Upload html pages to github
+    subprocess.call("git.bat " + github_account + " " + github_repo)
+
+def downloading_from_wayback():
+    # Create output folder, if it doesn't already exist
+    if not os.path.exists(path_temp_folder):
+        os.mkdir(path_temp_folder)
+    if not os.path.exists(path_vol_folder):
+        os.mkdir(path_vol_folder)
+    if not os.path.exists(path_json_folder):
+        os.mkdir(path_json_folder)
+    # Get snapshot links using Wayback's API
+    for i in range(files_count):
+        get_page("https://archive.org/wayback/available?url=https://" + github_account + ".github.io/" + github_repo + "/" + str(i) + ".html", path_json_folder + str(i) + ".html")
+        with open(path_json_folder + str(i) + ".html") as file:
+            jsondata = json.loads(file.read())
+            urls.append(jsondata["archived_snapshots"]["closest"]["url"].replace("/https://", "if_/https://"))
+    for url in urls:
+        print (url)
     # Download pages
     for i in range(len(urls)):
-        get_page(urls[i], output_folder + "/" + str(i) + ".txt")
-        print(urls[i] + "downloaded")
+        get_page(urls[i], path_vol_folder + str(i) + ".html")
+        print(urls[i], "downloaded")
     # Count html pages
-    html_files = os.listdir(path = output_folder)
+    html_files = os.listdir(path = path_vol_folder)
     file_number = len(html_files)
     # Join html pages into one ascii
-    join_files(file_number, downloaded_ascii, output_folder)
+    join_files(file_number, path_ascii, path_vol_folder)
     # Remove html pages
     for page in html_files:
-        os.remove(output_folder + "/" + page)
+        os.remove(path_vol_folder + page)
     # Get file from ascii
-    subprocess.run("decode.exe " + downloaded_ascii + " " + final_output)
+    subprocess.run("decode.exe " + path_ascii + " " + path_out)
     # Remove ascii
-    os.remove(downloaded_ascii)
+    os.remove(path_ascii)
     
